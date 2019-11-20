62e5a8e72b344f127e047ed201d6ba3dd92dabde
Antonio Dell'Elce <antouk@dellelce.com>
2019-11-20 14:13:59 +0000

    add welcome message

---
+++ .autoload.d/welcome.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# File:         welcome.sh
+# Created:      201119
+# Description:  description for welcome.sh
+#
+
+## MAIN ##
+
+cat << EOF
+ PEP 425 -- Compatibility Tags for Built Distributions
+EOF
+
+## EOF ##
